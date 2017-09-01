90a38f474d4a418c3f0a9ce5fc5e214e64931342
Pavel Dubrova <pashadubrova@gmail.com>
2017-09-01 16:42:21 +0300

    move array with our repo names to separate resource directory
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

---
+++ resources/reponame_array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+repos_array=([0]="platform_build" [1]="platform_external_toybox"
+             [2]="platform_frameworks_av" [3]="platform_frameworks_base"
+             [4]="platform_frameworks_native" [5]="platform_hardware_qcom_audio"
+             [6]="platform_hardware_qcom_bt" [7]="platform_hardware_qcom_display"
+             [8]="platform_hardware_qcom_gps" [9]="platform_hardware_qcom_keymaster"
+             [10]="platform_hardware_qcom_media" [11]="platform_packages_apps_Nfc"
+             [12]="platform_packages_apps_Settings" [13]="platform_packages_providers_DownloadProvider"
+             [14]="platform_packages_providers_MediaProvider" [15]="platform_system_core")
