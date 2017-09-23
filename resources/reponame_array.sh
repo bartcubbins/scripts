edc8e11c8828c0d3c7e1bdf8872c095ef48c1423
Pavel Dubrova <pashadubrova@gmail.com>
2017-09-23 10:49:40 +0300

    add new repos
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- resources/reponame_array.sh
+++ resources/reponame_array.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-repos_array=([0]="platform_build" [1]="platform_external_toybox"
-             [2]="platform_frameworks_av" [3]="platform_frameworks_base"
-             [4]="platform_frameworks_native" [5]="platform_hardware_qcom_audio"
-             [6]="platform_hardware_qcom_bt" [7]="platform_hardware_qcom_display"
-             [8]="platform_hardware_qcom_gps" [9]="platform_hardware_qcom_keymaster"
-             [10]="platform_hardware_qcom_media" [11]="platform_packages_apps_Nfc"
-             [12]="platform_packages_apps_Settings" [13]="platform_packages_providers_DownloadProvider"
-             [14]="platform_packages_providers_MediaProvider" [15]="platform_system_core")
+repos_array=([0]="platform_bionic" [1]="platform_build"
+             [2]="platform_build_soong" [3]="platform_external_toybox"
+             [4]="platform_frameworks_av" [5]="platform_frameworks_base"
+             [6]="platform_frameworks_native" [7]="platform_hardware_qcom_audio"
+             [8]="platform_hardware_qcom_bt" [9]="platform_hardware_qcom_display"
+             [10]="platform_hardware_qcom_gps" [11]="platform_hardware_qcom_keymaster"
+             [12]="platform_hardware_qcom_media" [13]="platform_packages_apps_Nfc"
+             [14]="platform_packages_apps_Settings" [15]="platform_packages_providers_DownloadProvider"
+             [16]="platform_packages_providers_MediaProvider" [17]="platform_system_core")
